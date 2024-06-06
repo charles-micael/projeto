d4b3207b63b5268f17097094e0493aa4d6c1c44b
Cliente <Cliente@DESKTOP-R0SKI6E>
2024-06-06 09:52:37 -0300

    criaçao do projeto

---
+++ projeto/src/curso/javaexecutavel/primeiroclaasejava.java
@@ -0,0 +1,13 @@
+package curso.javaexecutavel;
+
+public class primeiroclaasejava {
+	
+	/*main e um metodo auto executavel em java */
+	public static void main(String[] args) {
+		
+		/*escrever texto no console*/
+		System.out.println("meu primeiro programa em java ");
+		
+	}
+
+}
