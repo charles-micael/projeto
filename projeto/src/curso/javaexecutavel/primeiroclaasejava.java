d13f14db7ce47731959eaf924256f07f830483e6
charles_micael <charlesmicael.TI@gmail.com>
2024-06-16 19:30:30 -0300

    projeto while.java

--- projeto/src/curso/javaexecutavel/primeiroclaasejava.java
+++ projeto/src/curso/javaexecutavel/primeiroclaasejava.java
@@ -1,14 +1,52 @@
 package curso.javaexecutavel;
 
 public class primeiroclaasejava {
-	
-	/*main e um metodo auto executavel em java */
-	public static void main(String[] args) {
+	/*main é um metodo auto executado em Java*/
+	public static void main (String [] args) {
 		
-		/*escrever texto no console*/
-		System.out.println("meu primeiro programa em java ");
-		System.out.println("teste commit");
+		int nota1= 90;
+		int nota2= 90;
+
+		int key = 1;
+		
+				switch (key) {
+				case 1:
+					System.out.println("é 1");
+					break;
+				case 2:
+					System.out.println("é 2");
+					break;
+					
+				case 3:
+					System.out.println("é 3");
+					break;
+
+				default:
+					System.out.println("nenhuma das opções");
+					break;
+				}
+		
+		if (nota1!=nota2) {
+			System.out.println("As notas sao diferente");
+			
+		}else {
+			System.out.println("As notas sao iguais");
+			
+		}
+		
+		
+		if (nota1 <= 100) {
+			System.out.print("menor que 100");
+		}
+		
+		if(nota2 == 90) {
+			System.out.print("");
+		}
+		
+
 		
 	}
-
+	
 }
+
+	
